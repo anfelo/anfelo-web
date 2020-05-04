6b169edf202f3f0a3c5fe0efb20ad0fcf444b99d
Andres Osorio <af.osorio1341@gmail.com>
2020-05-04 18:17:07 -0500

    Add About page

---
+++ src/data/bio/bio.js
@@ -0,0 +1,6 @@
+export default {
+  name: 'Andres Osorio Bio',
+  shortBio:
+    "Hi, I'm a Fullstack JavaScript developer who loves the beauty of great design and the power of the web. I remember 5 years from now when I started learning about HTML, CSS and the web. I didn't realize it was going to become my career path and my passion. Today I spend my time building new projects with technologies like Angular, React and NodeJS. In my free time I like to read and learn about new web technologies and practice this concepts in side projects. I’m currently learning about PWAs and Svelte (a new JS framework).",
+  imgUrl: '/img/profile-pixel.png',
+};
