169654d709bb491f2b5f641df19cfd92cabdab61
Andres Osorio <af.osorio1341@gmail.com>
2020-05-05 19:37:51 -0500

    Fix access token

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@
 
 import * as contentful from 'contentful';
 
-const SPACE_ID = process.env.CONTENTFUL_SPACE_ID || '';
-const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN || '';
+const dev = process.env.NODE_ENV === 'development';
+const SPACE_ID = process.env.CONTENTFUL_SPACE_ID || '******';
+const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN || '******';
 
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
